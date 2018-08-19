9ac331fe84ac3e34c616e22b1eed83ca06c689a9
artur03121 <42422170+artur03121@users.noreply.github.com>
2018-08-19 08:12:41 -0300

    Add files via upload

--- problems/Test1.py
+++ problems/Test1.py
@@ -1,3 +1,27 @@
-a=input()
-b=input()
-print(int(a)+int(b))
+t=int(input())
+i=1
+j=1
+k=1
+k=int(k)
+arr2=[]
+test=[]
+arr3=[]
+while i<=t:
+    text=input()
+    arr1=list(text)
+    i=i+1
+    lenght=int(len(arr1))
+    while j<=lenght/2:
+        arr2.append(arr1[j-1])
+        j=j+1
+        l=len(arr2)
+    while k<=l:
+        if (int(k)-1)%2==0:
+            test.append(arr2[k-1])
+        k=k+1
+    print("".join(test))
+    arr1=[]
+    arr2=[]
+    test=[]
+    j=1
+    k=1
